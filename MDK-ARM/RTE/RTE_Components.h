b23f9ddaec2c0e1a977f235a1d83c05648166199
Catherine1147 <30961884+Catherine1147@users.noreply.github.com>
2017-08-24 11:40:20 +0300

    Initial

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'dmx_master' 
+ * Target:  'dmx_master' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
